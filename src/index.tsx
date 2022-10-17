6afb072845f97ddc65c265c5a3862a92bb6740fe
Afrokk <afrasiyabkhan379@gmail.com>
2022-10-17 03:30:20 -0400

    SASS Integration + CSSReset.

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Litmus from './Litmus'
 import reportWebVitals from './reportWebVitals';
+import './styles/globalStyles.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
