78f0c6ba383bf2cdf0bfca966781c7e908b8ff45
Usman Abdul Jabbar Shaikh <46794411+UsmanAJabbar@users.noreply.github.com>
2022-10-19 00:51:01 -0500

    Add InputField component (#8)
    * InputField Styles
    
    * Resolve merge conflict
    
    * Fix index.html
    
    * Cleanup styles
    
    * Quicksave
    
    * Add InputField props
    
    * Add InputField props
    
    * Update InputField Component
    
    * Reverted changes on index.html
    
    Co-authored-by: Afrokk <afrasiyabkhan379@gmail.com>

--- src/Litmus.tsx
+++ src/Litmus.tsx
@@ -4,5 +4,5 @@ const Litmus = (): JSX.Element => {
       <h1>Litmus.Tools</h1>
     </div>
   );
-}
+};
 export default Litmus;
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,6 +1,39 @@
-const InputField = (): JSX.Element => {
+import "./InputField.sass";
+
+type InputFieldProps = {
+  label: string;
+  type: string;
+  maxLength: number;
+  placeholder: string;
+  required: boolean;
+};
+
+const InputField = ({
+  label,
+  type,
+  maxLength,
+  placeholder,
+  required,
+}: InputFieldProps): JSX.Element => {
   return (
-    <></>
+    <div className="InputField">
+      <label>{label}</label>
+      <input
+        type={type}
+        maxLength={maxLength}
+        placeholder={placeholder}
+        required={required}
+      />
+    </div>
   );
 };
+
+InputField.defaultProps = {
+  label: "",
+  type: "text",
+  maxLength: 64,
+  placeholder: "",
+  required: false,
+};
+
 export default InputField;
