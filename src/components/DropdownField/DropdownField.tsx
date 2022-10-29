236ec79c06c11e1d4351e721008f1e1792e1093a
Afrasiyab Khan <74003001+Afrokk@users.noreply.github.com>
2022-10-29 03:09:14 -0400

    Built Dropdown Field Component (#9)
    * Built Dropdown Field Component
    
    * Error fixes.

---
+++ src/components/DropdownField/DropdownField.tsx
@@ -0,0 +1,33 @@
+import "./DropdownField.sass";
+
+type DropdownFieldProps = {
+  label: string;
+  id: string;
+  defaultSelected: string;
+  options: string[];
+};
+
+const DropdownField = ({
+  label,
+  id,
+  defaultSelected,
+  options,
+}: DropdownFieldProps): JSX.Element => {
+  return ( 
+    <div className="DropdownField">
+        <label>{label}</label>
+        <select id={id} defaultValue={defaultSelected}>
+            {options.map((o, idx) => <option key={idx} value={o}> {o} </option>)}
+        </select>
+    </div>
+    );
+};
+
+DropdownField.defaultProps = {
+  label: "",
+  id: "",
+  defaultSelected: undefined,
+  options: []
+};
+
+export default DropdownField;
