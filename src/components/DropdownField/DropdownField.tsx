ec80f9c36729c05a1f09607424e35e35ad30ba4f
Afrasiyab Khan <74003001+Afrokk@users.noreply.github.com>
2022-11-05 07:13:30 -0400

    Add Validation logic for DropdownField (#14)
    * Add Validation logic for DropdownField
    
    * Added mixins and update DropdownField
    
    * Cleanup Mixins

--- src/components/DropdownField/DropdownField.tsx
+++ src/components/DropdownField/DropdownField.tsx
@@ -1,11 +1,12 @@
 import "./DropdownField.sass";
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
 type DropdownFieldProps = {
   label: string;
   id: string;
   defaultSelected: string;
   options: string[];
+  required: boolean;
 };
 
 const DropdownField = ({
@@ -13,33 +14,56 @@ const DropdownField = ({
   id,
   defaultSelected,
   options,
+  required,
 }: DropdownFieldProps): JSX.Element => {
   let choices = [];
-  const [value, setValue] = useState(defaultSelected || '');
-  
-  function handleChange(e: any) {
+  const [value, setValue] = useState<string>(defaultSelected || "");
+  const [isTouched, setIsTouched] = useState<boolean>(false);
+  const [isValid, setIsValid] = useState<boolean>(true);
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setValue(e.target.value);
   }
 
+  const handleBlur = (): void => {
+    setIsTouched(true);
+  };
+
+  const validateOption = (): boolean => !required || !!value;
+
+  useEffect(() => {
+    setIsValid(validateOption());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value]);
+
   if (!defaultSelected) {
-    choices.push(<option key={0} value={""} hidden disabled> {""} </option>)
+    choices.push(<option key={0} value={""} hidden disabled></option>);
   }
   choices = [
     ...choices,
-    ...options.map((o, idx) => 
-      <option key={idx+1} value={o}> {o} </option>
-    )
-  ]
+    ...options.map((o, idx) => (
+      <option key={idx + 1} value={o}>
+        {" "}
+        {o}{" "}
+      </option>
+    )),
+  ];
   return (
-    <div className="dropdown-field-component">
-        <select 
-          id={id} 
-          defaultValue={defaultSelected} 
-          onChange={handleChange}
-        >
-          {choices}
-        </select>
-        <label className={(value && 'in-focus')}> {label} </label>
+    <div
+      className={`dropdown-field-component 
+    ${isValid && required ? "success" : isTouched && !isValid ? "error" : ""}
+    ${!!defaultSelected && isTouched ? "success" : ""}`}
+    >
+      <select
+        id={id}
+        defaultValue={defaultSelected}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        required={required}
+      >
+        {choices}
+      </select>
+      <label className={value && "in-focus"}> {label} </label>
     </div>
   );
 };
@@ -48,6 +72,7 @@ DropdownField.defaultProps = {
   id: "",
   defaultSelected: "",
   options: [],
+  required: false,
 };
 
 export default DropdownField;
