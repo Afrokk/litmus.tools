9b44f7c5820ecdcea3d6daa4b176b6094da7e4c3
Afrokk <afrasiyabkhan379@gmail.com>
2022-11-16 01:51:38 -0500

    Fixed bugs

--- src/Litmus.tsx
+++ src/Litmus.tsx
@@ -1,10 +1,7 @@
-import BudgetingList from "./components/BudgetingList/BudgetingList";
-
 const Litmus = (): JSX.Element => {
   return (
     <div>
       <h1>Litmus.Tools</h1>
-      <BudgetingList/>
     </div>
   );
 };
