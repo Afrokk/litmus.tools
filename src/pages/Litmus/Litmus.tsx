7d11e2fdfbb354e018cc6d8324f4790f7c8078ea
Afrokk <afrasiyabkhan379@gmail.com>
2022-12-12 01:25:06 -0500

    Create descriptions

--- src/pages/Litmus/Litmus.tsx
+++ src/pages/Litmus/Litmus.tsx
@@ -7,7 +7,6 @@ import "./Litmus.sass";
 
 type BudgetingData = Array<PresetBudgetItems>;
 
-//WIP
 const Litmus = (): JSX.Element => {
   const [userDetailsData, setUserDetailsData] = useState<UserData>({
     "Postal Code": "",
@@ -34,16 +33,42 @@ const Litmus = (): JSX.Element => {
       </div>
 
       <div className="secondary-details-container">
-        <div id="results">
+        <div id="descriptions">
           <p>
-            Some weak ass quote into to captivate the user. Only add the monthly charges of items you can't live without. <br /><br /> <br />
-            <strong>Note:</strong> This tool may not be super accurate.
+            Some weak ass quote into to captivate the user. Only add the monthly
+            charges of items you can't live without. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Aut exercitationem facere molestias
+            delectus inventore provident qui consequuntur veniam, corporis
+            architecto, fugit nesciunt odio error possimus eligendi tenetur
+            velit? Asperiores, aliquid.
           </p>
-          <div className="spaced-text result-headings">
-            <h2>Gross</h2>
-            <h2>Net</h2>
-            <h2>Taxes</h2>
-            <h3>(Gross - Net)</h3>
+          <div id="results" className="spaced-text capitalized-text">
+            <div className="result-heading">
+              <h2>Gross</h2>
+              <h3>$11966.11</h3>
+            </div>
+            <div className="result-heading">
+              <h2>Net</h2>
+              <h3>$9581.72</h3>
+            </div>
+            <div className="result-heading">
+              <h2>
+                Taxes<span>(Gross - Net)</span>
+              </h2>
+              <h3>$2891.56</h3>
+            </div>
+          </div>
+          <div className="synopsis">
+            <h2 className="spaced-text capitalized-text">Synopsis</h2>
+            <p>
+              You can save at max <span>$5681.11/month</span> when you have a
+              job and need atleast <span>$3631.11/month</span> to breakeven each
+              month. <br />
+              <br />
+              If you lose your job, you will need atleast{" "}
+              <span>$1831.11/month</span>. <br /> <br />
+              <span>Note:</span> This tool may not be super accurate.
+            </p>
           </div>
         </div>
         <div id="budgeting-list">
