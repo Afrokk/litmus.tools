f4f084f37beff347c7f2ff3d06e372e0449f87ec
Afrokk <afrasiyabkhan379@gmail.com>
2023-01-01 06:55:49 -0500

    add Taxim

--- api/scripts/1_setup_python_310.sh
+++
@@ -1,6 +0,0 @@
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
-sudo apt install python3.10
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
-echo "Now run 'sudo update-alternatives --config python3'"
